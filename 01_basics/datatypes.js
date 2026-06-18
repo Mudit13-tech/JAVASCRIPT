2e9b3fabd1722201f0db24c518c3a7f2d688c93c
Mudit Golchha <muditgolchha35@gmail.com>
2026-06-18 12:01:51 +0000

    DATATYPES COMPLETED

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS code as newer version  
+/*
+string 
+boolean
+null - standalone value 
+undefined
+symbol - unique
+*/
+
+// object - imp
+
+console.log(typeof null); 
+// null is a type of object 
+
+console.log(typeof undefined);
+// type of undefined is undefined
