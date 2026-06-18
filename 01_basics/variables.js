cca2c09b204ec2fe8939102b059990ac8e85f9d6
Mudit Golchha <muditgolchha35@gmail.com>
2026-06-18 11:47:03 +0000

    VARIABLES.

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const account_ID = 1445563 //const is fixed it cant be changed 
+let accountEmail = "muditgolchha35@gmail.com" // let can be changed 
+var accountPassword = "123456"
+accountCity = "Jaipur"
+
+accountEmail = "hchch@gmail.com"
+accountPassword = "1222"
+accountCity = "Hyderabad"
+
+/* 
+prefer not to use var because of issue in block scope and functional scope 
+*/
+
+console.table([account_ID,accountEmail,accountPassword,accountCity])
