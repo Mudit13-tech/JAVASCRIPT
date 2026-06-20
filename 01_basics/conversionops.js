72bd53949431c1384c8ba8403ea0c52d74f40bfe
Mudit Golchha <muditgolchha35@gmail.com>
2026-06-20 17:22:15 +0000

    CONVERSIONS 01_basics

---
+++ 01_basics/conversionops.js
@@ -0,0 +1,20 @@
+let score = "54"
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33ABC" => NaN (not a number )
+// true => 1 and false =>0
+
+let isLoggedIN = 1;
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(booleanIsLoggedIn);
+
+/* if isLoggedIn:
+ "" => false
+ hitesh or any other string = true 
+
+*/
